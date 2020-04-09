2fe16cf40084e43042003887a34c3df0ab8e2450
Kennedy-lab <ken96odhiambo@gmail.com>
2020-04-09 15:49:42 +0300

    first commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,15 @@
-const covid19ImpactEstimator = (data) => data;
-
+const covid19ImpactEstimator = (data) => { 
+        const impact = () => {
+            const reportedCases;
+            const currentlyInfected = reportedCases*10;
+            const threeDayPeriod;
+            const infectionsByRequestedTime = currentlyInfected * Math.pow(2,threeDayPeriod);
+        }
+        const severeImpact = () => {
+            const reportedCases;
+            const currentlyInfected = reportedCases*50;
+            const threeDayPeriod;
+            const infectionsByRequestedTime = currentlyInfected * Math.pow(2,threeDayPeriod);
+        }
+}
 export default covid19ImpactEstimator;

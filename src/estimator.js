bc705786f207b56b6bbf80cd4230acc98cfb4703
Kennedy-lab <ken96odhiambo@gmail.com>
2020-04-13 11:11:48 +0300

    updated trunc function

--- src/estimator.js
+++ src/estimator.js
@@ -2,11 +2,11 @@ const covid19ImpactEstimator = (data) => {
   const duration = () => {
     let numberOfThreeDayPeriods;
     if (data.periodType === 'days') {
-      numberOfThreeDayPeriods = Math.trunc(data.timeToElapse / 3);
+      numberOfThreeDayPeriods = (data.timeToElapse / 3);
     } else if (data.periodType === 'weeks') {
-      numberOfThreeDayPeriods = Math.trunc((data.timeToElapse * 7) / 3);
+      numberOfThreeDayPeriods = ((data.timeToElapse * 7) / 3);
     } else if (data.periodType === 'months') {
-      numberOfThreeDayPeriods = Math.trunc((data.timeToElapse * 30) / 3);
+      numberOfThreeDayPeriods = ((data.timeToElapse * 30) / 3);
     }
     return numberOfThreeDayPeriods;
   };
@@ -14,12 +14,12 @@ const covid19ImpactEstimator = (data) => {
     const currentlyInfected = data.reportedCases * 10;
     const days = duration();
     const factor = 2 ** days;
-    const infectionsByRequestedTime = currentlyInfected * factor;
+    const infectionsByRequestedTime = Math.trunc(currentlyInfected * factor);
     const severeCasesByRequestedTime = Math.trunc((15 / 100) * infectionsByRequestedTime);
-    const availableBeds = Math.trunc((35 / 100) * data.totalHospitalBeds);
-    const hospitalBedsByRequestedTime = availableBeds - severeCasesByRequestedTime;
+    const availableBeds = (35 / 100) * data.totalHospitalBeds;
+    const hospitalBedsByRequestedTime = Math.trunc(availableBeds - severeCasesByRequestedTime);
     const casesForICUByRequestedTime = Math.trunc((5 / 100) * infectionsByRequestedTime);
-    const casesForVentilatorsByRequestedTime = (2 / 100) * infectionsByRequestedTime;
+    const casesForVentilatorsByRequestedTime = Math.trunc((2 / 100) * infectionsByRequestedTime);
     const expectedAmount = data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD;
     const loss = infectionsByRequestedTime * expectedAmount;
     const dollarsInFlight = Math.trunc(loss / days);
@@ -37,10 +37,10 @@ const covid19ImpactEstimator = (data) => {
     const currentlyInfected = data.reportedCases * 50;
     const days = duration();
     const factor = 2 ** days;
-    const infectionsByRequestedTime = currentlyInfected * factor;
+    const infectionsByRequestedTime = Math.trunc(currentlyInfected * factor);
     const severeCasesByRequestedTime = Math.trunc((15 / 100) * infectionsByRequestedTime);
-    const availableBeds = Math.trunc((35 / 100) * data.totalHospitalBeds);
-    const hospitalBedsByRequestedTime = availableBeds - severeCasesByRequestedTime;
+    const availableBeds = (35 / 100) * data.totalHospitalBeds;
+    const hospitalBedsByRequestedTime = Math.trunc(availableBeds - severeCasesByRequestedTime);
     const casesForICUByRequestedTime = Math.trunc((5 / 100) * infectionsByRequestedTime);
     const casesForVentilatorsByRequestedTime = Math.trunc((2 / 100) * infectionsByRequestedTime);
     const expectedAmount = data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD;
